322b64fdcb01b1e5ece1c883d0bb8c02d21f098a
Nastya <79398055+tisenres@users.noreply.github.com>
2023-07-18 20:58:34 +0500

    Create Owner.sol
    Init basic file for Smart Contract

---
+++ Owner.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract Owner {
+    uint public receiversNumber;
+    uint public bank;
+    address public owner;
+
+    struct Receiver {
+        string name;
+        uint256 birthday;
+        bool alreadyGotMoney;
+        bool exitst;
+    }
+
+    address[] public arrReceivers;
+
+    mapping(address => Receiver) public receivers;
+
+    constructor () {
+        owner = msg.sender;
+        receiversNumber = 0;
+    }
+
+    modifier onlyOwner() {
+        require (msg.sender == owner, "You are not owner");
+        _;
+    }
+
+    function addReceiver (
+        address walletAddress,
+        string memory name,
+        uint256 birthday
+        ) public onlyOwner
+    {
+        require (birthday > 0, "Something is wrong with birhday!");
+        require (receivers[walletAddress].exitst == false, "This receiver already exists!");
+
+        receivers[walletAddress] = (
+            Receiver(name, birthday, false, true)
+        );
+
+        arrReceivers.push(walletAddress);
+        receiversNumber++;
+
+        emit NewGrandChild(walletAddress, name, birthday);
+    }
+
+    function recieve() external payable {
+        bank += msg.value;
+    }
+
+    function balanceOf() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function withdraw() public {
+        address payable walletAddress = payable(msg.sender);
+
+        require(
+            receivers[walletAddress].exitst == true,
+            "There is no such grandchild!"
+        );
+
+        require(
+            block.timestamp >= receivers[walletAddress].birthday,
+            "Birthday hasn't arrived yet!"
+        );
+
+        require(
+            receivers[walletAddress].alreadyGotMoney == false,
+            "You have already recieved money!"
+        );
+
+        uint256 amount = bank / receiversNumber;
+        receivers[walletAddress].alreadyGotMoney = true;
+
+        (bool success, ) = walletAddress.call {value: amount} ("");
+        require(success);
+
+        emit GotMoney(walletAddress);
+    }
+
+    function readGrandChildsArray(uint cursor, uint length) public view returns(address[] memory) {
+        address[] memory array = new address[](length);
+        uint counter = 0;
+
+        for (uint i = cursor; i < cursor + length; i++) {
+            array[counter] = arrReceivers[i];
+            counter++;
+        }
+
+        return array;
+    }
+
+    event NewGrandChild(address indexed walletAddress, string name, uint256 birthday);
+    event GotMoney(address indexed walletAddress);
+}
